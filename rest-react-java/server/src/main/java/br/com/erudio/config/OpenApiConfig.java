ff28688b78d76b24d0ae24f51490e9ef7758fac1
leandrocgsi <leandrocgsi@gmail.com>
2024-02-03 15:36:38 -0300

    Continuous Integration with Github, Github Actions and Docker Hub

--- rest-react-java/server/src/main/java/br/com/erudio/config/OpenApiConfig.java
+++ rest-react-java/server/src/main/java/br/com/erudio/config/OpenApiConfig.java
@@ -9,15 +9,21 @@ import io.swagger.v3.oas.models.info.License;
 
 @Configuration
 public class OpenApiConfig {
-
+	
 	@Bean
-	public OpenAPI customOpenApi() {
+	public OpenAPI customOpenAPI() {
 		return new OpenAPI()
-		        .info(new Info()
-		                .title("RESTful API With Java 16 and Spring Boot 2.6")
-		                .version("v1")
-		                .description("Some description about your API.")
-		                .termsOfService("https://pub.erudio.com.br/meus-cursos")
-		                .license(new License().name("Apache 2.0").url("https://pub.erudio.com.br/meus-cursos")));
+			.info(new Info()
+				.title("REST API's RESTful from Zero to AWS with Java 21, Spring Boot 3.2.0 and Docker")
+				.version("v1")
+				.description("Some description about your API")
+				.termsOfService("https://pub.erudio.com.br/meus-cursos")
+				.license(
+					new License()
+						.name("Apache 2.0")
+						.url("https://pub.erudio.com.br/meus-cursos")
+					)
+				);
 	}
+
 }
