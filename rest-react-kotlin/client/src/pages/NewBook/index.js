c413d54d9a15da4be9b517be0a9ec455ac51a93d
leandrocgsi <leandrocgsi@gmail.com>
2024-02-04 14:42:42 -0300

    Adding Server API in Spring Boot 3.2.0 and Kotlin 1.9.10

---
+++ rest-react-kotlin/client/src/pages/NewBook/index.js
@@ -0,0 +1,115 @@
+import React, {useState, useEffect} from 'react';
+import {useNavigate, useParams, Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi'
+
+import api from '../../services/api'
+
+import './styles.css';
+
+import logoImage from '../../assets/logo.svg';
+
+export default function NewBook(){
+
+    const [id, setId] = useState(null);
+    const [author, setAuthor] = useState('');
+    const [launchDate, setLaunchDate] = useState('');
+    const [price, setPrice] = useState('');
+    const [title, setTitle] = useState('');
+
+    const { bookId } = useParams();
+
+    const navigate = useNavigate();
+    
+    const accessToken = localStorage.getItem('accessToken');
+
+    const authorization = {
+        headers: {
+            Authorization: `Bearer ${accessToken}`
+        }
+    };
+
+    useEffect(() => {
+        if (bookId === '0') return;
+        else loadBooks();
+    }, [bookId]);
+
+    async function loadBooks() {
+        try {
+            const response = await api.get(`api/book/v1/${bookId}`, authorization) ;
+
+            let adjustedDate = response.data.launchDate.split("T", 10)[0];
+
+            setId(response.data.id);
+            setTitle(response.data.title);
+            setAuthor(response.data.author);
+            setPrice(response.data.price);
+            setLaunchDate(adjustedDate);
+        } catch (err) {
+            alert('Error recovering Book! Try again!');
+            navigate('/books');
+        }
+    }
+
+    async function saveOrUpdate(e){
+        e.preventDefault();
+
+        const data = {
+            author,
+            launchDate,
+            price,
+            title
+        };
+
+        try {
+            if (bookId === '0') {
+                await api.post('api/book/v1', data, authorization) ;
+            } else {
+                data.id = bookId;
+                await api.put('api/book/v1', data, authorization) ;
+            }
+        } catch (error) {
+            alert('Login failed! Try again!');
+        }
+        navigate('/books');
+    };
+
+    return (
+        <div className="new-book-container">
+            <div className="content">
+                <section className="form">
+                    <img src={logoImage} alt="Erudio"/>
+                    <h1>{bookId === '0' ? 'Add New' : 'Update'} Book</h1>
+                    <p>Enter the book information and click on {bookId === '0' ? `'Add New'` : `'Update'`}!</p>
+                    <Link className="back-link" to="/books">
+                        <FiArrowLeft size={16} color="#251fc5"/>
+                        Home
+                    </Link>
+                </section>
+                <form onSubmit={saveOrUpdate}>
+                    <input
+                        placeholder="Title"
+                        value={title}
+                        onChange={e => setTitle(e.target.value)}
+                    />
+                    <input
+                        placeholder="Author"
+                        value={author}
+                        onChange={e => setAuthor(e.target.value)}
+                    />
+                    <input
+                        type="date"
+                        value={launchDate}
+                        onChange={e => setLaunchDate(e.target.value)}
+                    />
+                    <input
+                        placeholder="Price"
+                        value={price}
+                        onChange={e => setPrice(e.target.value)}
+                    />
+
+                    <button className="button" type="submit">{bookId === '0' ? 'Add' : 'Update'}</button>
+                </form>
+            </div>
+        </div>
+    );
+}
